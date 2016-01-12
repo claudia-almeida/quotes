a66f7b168af9bc5143903663906163176adc02e0
Claudia Almeida <ihavenids@gmail.com>
2016-01-12 16:21:18 +0000

    Last commit before Heroku

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,5 @@
 class Quote < ActiveRecord::Base
-  validates :saying, :presence => true, :length => { in: 3..150 }
+  validates :saying, :presence => true, :length => { in: 3..200 }
   validates :author, :presence => true, :length => { in: 3..25 }
 end
 
