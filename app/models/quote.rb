390f627d602de2179644314b0b4e72bf87b1f512
Claudia Almeida <ihavenids@gmail.com>
2016-01-13 15:24:24 +0000

    Solving mobile issue...

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,5 @@
 class Quote < ActiveRecord::Base
-  validates :saying, :presence => true, :length => { in: 3..200 }
+  validates :saying, :presence => true, :length => { in: 60..200 }
   validates :author, :presence => true, :length => { in: 3..25 }
 end
 
